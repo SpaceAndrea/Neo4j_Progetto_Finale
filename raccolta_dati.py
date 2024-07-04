f3b26d7853200e6ff3eafa7d10a740d35e5b9ccc
SpaceAndrea <business.andreabianchini@gmail.com>
2024-07-05 01:20:37 +0200

    fix vari +  aggiunta file test main.py: -Ho aggiunto nel main l'importazione del file json, il quale viene creato dal modulo 'raccolta_dati.py' (possibilmente da automatizzare). -Ho creato i nodi per le Persone usando i dati che si trovano nel file creatosi.

--- raccolta_dati.py
+++ raccolta_dati.py
@@ -5,11 +5,11 @@ def raccogli_dati_persona():
         #informazioni personali
         "nome": input("Inserisci il nome: "),
         "cognome": input("Inserisci il cognome: "),
-        "età": int(input("Inserisci l'età: ")),
+        "eta": int(input("Inserisci l'età: ")),
         "data_di_nascita": input("Inserisci la data di nascita con un formato YYYY-MM-DD: "),
         #indirizzo
         "via": input("Inserisci la via: "),
-        "città": input("Inserisci la città: "),
+        "citta": input("Inserisci la città: "),
         "cap": input("Inserisci il CAP: "),
         #contatti
         "email": input("Inserisci l'email: "),
